9f8dd1e2292d21ecd5ff3413ff352b7164f8374b
riveraaj <jonathandavidr7@gmail.com>
2024-11-02 14:00:06 -0600

    [BUGFIX] Add log to git

---
+++ DosPinos.HRMS.BusinessObjects/POCOs/Commons/Log/LogPOCO.cs
@@ -0,0 +1,12 @@
+﻿namespace DosPinos.HRMS.BusinessObjects.POCOs.Commons.Log
+{
+    public class LogPOCO : EntityDTO, ILogPOCO
+    {
+        public DateTime TimeStamp { get; set; }
+        public string Source { get; set; }
+        public string Message { get; set; }
+        public string Exeption { get; set; }
+        public ActionCategory Action { get; set; }
+        public Module Module { get; set; }
+    }
+}
